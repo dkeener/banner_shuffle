a3e6d8469ab3093b2aa3d99937fb11bb1d7722c4
David Keener <dkeener@keenertech.com>
2026-08-31 11:25:03 -0400

    Added a starter set of library book cover images.

---
+++ custom_pools/lib.js
@@ -0,0 +1,24 @@
+// LIB — Book cover images from David Keener's library.
+
+
+// Book Covers (150 x 250).
+var pub_lib = [
+["https://www.amazon.com/dp/TBD/",
+    "https://gallery.davidkeener.org/lib/1633.jpg",
+    "1633"],
+["https://www.amazon.com/dp/TBD/",
+    "https://gallery.davidkeener.org/lib/agent_of_the_imperium.jpg",
+    "Agent of the Imperium"],
+["https://www.amazon.com/dp/TBD/",
+    "https://gallery.davidkeener.org/lib/investigator.jpg",
+    "The Investigator"],
+["https://www.amazon.com/dp/TBD/",
+    "https://gallery.davidkeener.org/lib/lonesome_october.jpg",
+    "A Night in the Lonesome October"],
+["https://www.amazon.com/dp/TBD/",
+    "https://gallery.davidkeener.org/lib/mamelukes.jpg",
+    "Mamelukes"],
+["https://www.amazon.com/dp/TBD/",
+    "https://gallery.davidkeener.org/lib/polymath.jpg",
+    "Polymath"]
+];
