50ea73c7668e65b36dd21ac25b30d4830a2e4bd6
David Keener <dkeener@keenertech.com>
2026-08-31 16:44:04 -0400

    Added DK's 150x250 book cover images.

---
+++ custom_pools/pool_mybooks.js
@@ -0,0 +1,37 @@
+// MYBOOKS — 150x250 book cover images for David Keener's books.
+
+// Waiting for publication
+// - death_comes_to_town.png
+// - run_hard_run_fast.png
+// - winter_spirits.png
+
+// Small Book Covers (150x250 pixels) - For thumbnails and panel displays.
+var pub_mybooks_big = [
+    ["https://www.amazon.com/dp/B0C43RDGHN/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/bitter_days.png",
+        "Bitter Days"],
+    ["https://www.amazon.com/dp/B09PF8JLV7/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/clash_by_night.png",
+        "Clash by Night"],
+    ["https://www.amazon.com/dp/B0DGWM1T79/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/dustbowl_detective.png",
+        "Dustbowl Detective"],
+    ["https://worldsenough.square.site/product/jakarta-breach-exclusive-edition-/59",
+        "https://gallery.davidkeener.org/books/jakarta_breach.jpg",
+        "The Jakarta Breach"],
+    ["https://www.amazon.com/dp/B0CTNNZSW4/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/jonelle_crosse.png",
+        "Jonelle Crosse"],
+    ["https://www.amazon.com/dp/B09P841FTS/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/road_trip.jpg",
+        "Road Trip"],
+    ["https://www.amazon.com/dp/B07JHTFKJY?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/rooftop_game.jpg",
+        "The Rooftop Game"],
+    ["https://www.amazon.com/dp/B08XBYV993?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/unlikely_hero.png",
+        "An Unlikely Hero"],
+    ["https://www.amazon.com/dp/B0825F2FC1?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books/whispering_voice.png",
+        "The Whispering Voice"]
+];
