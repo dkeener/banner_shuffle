897f52a67fc8942388665ea3af4872eb81aadc68
David Keener <dkeener@keenertech.com>
2026-08-31 15:10:59 -0400

    Replaced an image, plus set up the pool to use the Nginx Image Server.

--- custom_pools/pool_features.js
+++ custom_pools/pool_features.js
@@ -4,12 +4,12 @@
 // Featured sidebar images (320 - 360) x (188 - 220).
 var pub_features = [
 ["https://www.amazon.com/dp/B0BRL5RGC9/",
-    "https://ragnarok.davidkeener.org/wp-content/uploads/2024/10/ad_wefd2_jonelle_crosse_sm-2.png",
+    "https://gallery.davidkeener.org/side/fd2_jonelle_crosse.png",
     "Jonelle Crosse"],
 ["",
-    "https://ragnarok.davidkeener.org/wp-content/uploads/2024/10/ukraine_supporter-1.jpeg",
+    "https://gallery.davidkeener.org/side/ukraine_supporter.jpg",
     "We Support Ukraine!"],
-["https://www.ravencon.com",
-    "https://www.davidkeener.org/wp-content/uploads/2024/01/ravencon_2024_david_keener_ad.jpg",
-    "Ravencon, featuring David Keener"]
+["https://www.davidkeener.org/about",
+    "https://gallery.davidkeener.org/side/dk_auth_ed_speaker.png",
+    "David Keener: Author, Editor, Speaker"]
 ];
