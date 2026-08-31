753ed1b9dc9842e92666c1a0018002610de9524f
David Keener <dkeener@keenertech.com>
2026-08-31 10:10:41 -0400

    Added specific pools for use on David Keener's websites.

---
+++ custom_pools/features.js
@@ -0,0 +1,15 @@
+// FEATURES — Featured ad images, usually for events or new book releases.
+
+
+// Featured sidebar images (320 - 360) x (188 - 220).
+var pub_features = [
+["https://www.amazon.com/dp/B0BRL5RGC9/",
+    "https://ragnarok.davidkeener.org/wp-content/uploads/2024/10/ad_wefd2_jonelle_crosse_sm-2.png",
+    "Jonelle Crosse"],
+["",
+    "https://ragnarok.davidkeener.org/wp-content/uploads/2024/10/ukraine_supporter-1.jpeg",
+    "We Support Ukraine!"],
+["https://www.ravencon.com",
+    "https://www.davidkeener.org/wp-content/uploads/2024/01/ravencon_2024_david_keener_ad.jpg",
+    "Ravencon, featuring David Keener"]
+];
---
+++ custom_pools/mybooks_big.js
@@ -0,0 +1,33 @@
+// MYBOOKS_BIG — 500x800 book cover images for David Keener's books.
+
+
+// Big Book Covers (500x800 pixels) - For a Book Feature display.
+var pubs_mybooks_big = [
+    ["https://www.amazon.com/dp/B0C43RDGHN/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/bitter_days.png",
+        "Bitter Days"],
+    ["https://www.amazon.com/dp/B09PF8JLV7/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/clash_by_night.png",
+        "Clash by Night"],
+    ["https://www.amazon.com/dp/B0DGWM1T79/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/dustbowl_detective.png",
+        "Dustbowl Detective"],
+    ["https://worldsenough.square.site/product/jakarta-breach-exclusive-edition-/59",
+        "https://gallery.davidkeener.org/books800/jakarta_breach.png",
+        "The Jakarta Breach"],
+    ["https://www.amazon.com/dp/B0CTNNZSW4/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/jonelle_crosse.png",
+        "Jonelle Crosse"],
+    ["https://www.amazon.com/dp/B09P841FTS/?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/road_trip.png",
+        "Road Trip"],
+    ["https://www.amazon.com/dp/B07JHTFKJY?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/rooftop_game.png",
+        "The Rooftop Game"],
+    ["https://www.amazon.com/dp/B08XBYV993?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/unlikely_hero.png",
+        "An Unlikely Hero"],
+    ["https://www.amazon.com/dp/B0825F2FC1?tag=keenertech-20",
+        "https://gallery.davidkeener.org/books800/whispering_voice.png",
+        "The Whispering Voice"]
+];
