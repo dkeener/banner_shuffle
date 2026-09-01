e7adb423ce1e830a266a3daa5fe3fc21debc7ba7
David Keener <dkeener@keenertech.com>
2026-09-01 14:27:11 -0400

    Added Amazon links for the library books.

--- custom_pools/pool_lib.js
+++ custom_pools/pool_lib.js
@@ -3,22 +3,22 @@
 
 // Book Covers (150 x 250).
 var pub_lib = [
-["https://www.amazon.com/dp/TBD/",
+["https://www.amazon.com/dp/B00AP8YZI0/?tag=keenertech-20",
     "https://gallery.davidkeener.org/lib/1633.jpg",
     "1633"],
-["https://www.amazon.com/dp/TBD/",
+["https://www.amazon.com/dp/B08MLNK1NK/?tag=keenertech-20",
     "https://gallery.davidkeener.org/lib/agent_of_the_imperium.jpg",
     "Agent of the Imperium"],
-["https://www.amazon.com/dp/TBD/",
+["https://www.amazon.com/dp/B0997VH2VP/?tag=keenertech-20",
     "https://gallery.davidkeener.org/lib/investigator.jpg",
     "The Investigator"],
-["https://www.amazon.com/dp/TBD/",
+["https://www.amazon.com/dp/B0CS2J126H/?tag=keenertech-20",
     "https://gallery.davidkeener.org/lib/lonesome_october.jpg",
     "A Night in the Lonesome October"],
-["https://www.amazon.com/dp/TBD/",
+["https://www.amazon.com/dp/B086T5Y5FP/?tag=keenertech-20",
     "https://gallery.davidkeener.org/lib/mamelukes.jpg",
     "Mamelukes"],
-["https://www.amazon.com/dp/TBD/",
+["https://www.amazon.com/dp/B00H6SOTL0/?tag=keenertech-20",
     "https://gallery.davidkeener.org/lib/polymath.jpg",
     "Polymath"]
 ];
