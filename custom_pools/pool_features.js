7adf0efcb3308af37910b674925d2bdc88ec3b9f
David Keener <dkeener@keenertech.com>
2026-09-02 22:52:51 -0400

    Added ad for Bluemont Fair 2026.

--- custom_pools/pool_features.js
+++ custom_pools/pool_features.js
@@ -3,13 +3,16 @@
 
 // Featured sidebar images (320 - 360) x (188 - 220).
 var pub_features = [
+["https://www.bluemontfair.org",
+    "https://gallery.davidkeener.org/side/bluemont_fair_2026.png",
+    "56th Annual Bluemont Fair, Sep 19 - 20, 2026"],
+["https://www.davidkeener.org/about",
+    "https://gallery.davidkeener.org/side/dk_auth_ed_speaker.png",
+    "David Keener: Author, Editor, Speaker"],
 ["https://www.amazon.com/dp/B0BRL5RGC9/",
     "https://gallery.davidkeener.org/side/fd2_jonelle_crosse.png",
     "Jonelle Crosse"],
 ["",
     "https://gallery.davidkeener.org/side/ukraine_supporter.jpg",
-    "We Support Ukraine!"],
-["https://www.davidkeener.org/about",
-    "https://gallery.davidkeener.org/side/dk_auth_ed_speaker.png",
-    "David Keener: Author, Editor, Speaker"]
+    "We Support Ukraine!"]
 ];
